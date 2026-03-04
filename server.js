f3ec80211a37c223d89e73e68d520b7e2af76bb4
Adekanbi Akorede <adekanbiakorede007@gmail.com>
2026-03-04 12:13:43 -0800

    still learning

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express');
+app.get("/", (req, res) => {
+  res.send("Hello from Express");
 });
 
+app.get("/about", (req, res) => {
+  res.send("This is the About page.");
+});
+
+app.get("/product", (req, res) => {
+  res.send([
+    { id: 1, name: "laptop", Price: 2000 },
+    { id: 2, name: "mouse", Price: 1000 },
+  ]);
+});
+
+app.get("/product/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const product = [
+    { id: 1, name: "laptop", Price: 2000 },
+    { id: 2, name: "mouse", Price: 1000 },
+  ];
+
+  const requestedProduct = product.find((product) => product.id === id)
+  res.json(requestedProduct)
+});
+
+app.get('/message', (req, res) => {
+  res.json({message: 'Hello from korede server'})
+})
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
